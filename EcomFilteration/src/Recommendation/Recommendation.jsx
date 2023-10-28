266a7499b05e3679d65faf0f43bd5664c30e9595
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2023-10-28 20:12:51 +0530

    Recommendations design done

--- EcomFilteration/src/Recommendation/Recommendation.jsx
+++ EcomFilteration/src/Recommendation/Recommendation.jsx
@@ -2,7 +2,18 @@ import "./Recommendation.css"
 
 const Recommendation = () => {
   return (
-    <div>Recommendation</div>
+    <>
+    <div>
+      <h2 className="recommended-title">Recommended</h2>
+      <div className="recommended-flex">
+        <button className="btns">All Products</button>
+        <button className="btns">Nike</button>
+        <button className="btns">Adidas</button>
+        <button className="btns">Puma</button>
+        <button className="btns">Jaguar</button>
+      </div>
+    </div>
+    </>
   )
 }
 
