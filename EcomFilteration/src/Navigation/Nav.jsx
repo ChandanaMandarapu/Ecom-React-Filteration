01731a2216f84fd52a18d7e19cc5306cc6e269c2
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2023-10-28 18:42:56 +0530

    Navbar done

--- EcomFilteration/src/Navigation/Nav.jsx
+++ EcomFilteration/src/Navigation/Nav.jsx
@@ -1,8 +1,28 @@
+import {FiHeart} from 'react-icons/fi'
+import {AiOutlineShoppingCart,AiOutlineUserAdd} from 'react-icons/ai'
 import './Nav.css'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <div className="nav-container">
+        <input type="text" 
+        placeholder='Enter your search items'
+        className='search-input' 
+        />
+      </div>
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className='nav-icons' />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className = "nav-icons" />
+        </a>
+        <a href="#">
+        <AiOutlineUserAdd className='nav-icons'/>
+        </a>
+      </div>
+    </nav>
   )
 }
 
